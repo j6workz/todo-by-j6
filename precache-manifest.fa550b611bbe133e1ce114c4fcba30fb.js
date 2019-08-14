7267f21f54c497ef70da9115c5a1134e7e42752a
j6workz <j6workz@gmail.com>
2019-08-14 11:18:05 +0530

    Updates

---
+++ precache-manifest.fa550b611bbe133e1ce114c4fcba30fb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f108efb6cac3953fda266bfb5cdd8f40",
+    "url": "/todo-by-j6/index.html"
+  },
+  {
+    "revision": "b2366ad6cb395d4a8680",
+    "url": "/todo-by-j6/static/css/main.b718d05e.chunk.css"
+  },
+  {
+    "revision": "bc7720a7c8d82b72777d",
+    "url": "/todo-by-j6/static/js/2.6ae6b944.chunk.js"
+  },
+  {
+    "revision": "b2366ad6cb395d4a8680",
+    "url": "/todo-by-j6/static/js/main.ff6460ae.chunk.js"
+  },
+  {
+    "revision": "429e69e201eafc3a4653",
+    "url": "/todo-by-j6/static/js/runtime~main.12790671.js"
+  }
+]);
